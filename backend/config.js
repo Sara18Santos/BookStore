f64b3f5952ec27106e42f33cbaf9332bdcf12cd3
Sara18Santos <sataide.santos@gmail.com>
2025-09-08 18:24:42 +0100

    feat(model): routes for new book and get all books

--- backend/config.js
+++ backend/config.js
@@ -1,1 +1,7 @@
-export const PORT = 5001;
+
+import dotenv from "dotenv";
+
+dotenv.config();
+
+export const PORT = process.env.PORT || 5000;
+export const MONGO_URL = process.env.MONGO_URL;
--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,59 @@
 import express from "express";
-import {PORT} from "./config.js"
+import mongoose from "mongoose";
+import { PORT, MONGO_URL } from "./config.js";
+import {Book } from "./models/bookmodel.js";
 
 const app = express();
 
-app.listen(PORT,()=>{
-    console.log(`App is listening to port: ${PORT}`);
-})
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    return res.status(200).send("MERN");
+});
+
+//Route for save a new book
+app.post("/books", async (req, res) => {
+    try {
+        if (!req.body.title || !req.body.author || !req.body.publishYear) {
+            return res.status(400).send({message: "Send all required fields: title, author, publishYear",});
+        }
+        const newBook = {
+            title: req.body.title, 
+            author: req.body.author,
+            publishYear: req.body.publishYear,
+        };
+        const book = await Book.create(newBook);
+
+        return res.status(201).send(book);
+    } catch (error) {
+        console.log(error.message);
+        res.status(500).send({message: error.message});
+    }
+});
+// Get All Books 
+app.get("/books", async (req, res) => {
+    try {
+        const books = await Book.find({});
+
+        return res.status(200).json({
+            count: books.length,
+            data: books
+        });
+    } catch (error) {
+        console.log(error.message);
+        res.status(500).send({message: error.message});
+    }
+});
+
+
+mongoose
+    .connect(MONGO_URL)
+    .then(() => {
+        console.log("Connected to MongoDB");
+        app.listen(PORT, () => {
+            console.log(`App is listening on port: ${PORT}`);
+        });
+    })
+    .catch((error) => {
+        console.error("MongoDB connection failed:", error.message);
+    });
