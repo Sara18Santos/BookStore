23b4a5e7523f81234d9a8cce0a77b4269de61826
Sara18Santos <sataide.santos@gmail.com>
2025-09-10 11:06:50 +0100

    feat: add routes PUT and DELETE, add folder routes

--- backend/index.js
+++ backend/index.js
@@ -1,50 +1,30 @@
 import express from "express";
 import mongoose from "mongoose";
+import cors from "cors";
 import { PORT, MONGO_URL } from "./config.js";
-import {Book } from "./models/bookmodel.js";
+import {Book} from './models/bookModel.js';
+import routesBooks from './routes/routesBooks.js'
 
 const app = express();
 
 app.use(express.json());
 
+// CORS Policy
+// app.use(cors());
+app.use(
+    cors({
+        origin: 'http://localhost:3000',
+        methods: ['GET', 'POST', 'PUT', 'DELETE'],
+        allowedHeaders: ['Content-Type'],
+    })
+);
+
 app.get("/", (req, res) => {
-    return res.status(200).send("MERN");
+    console.log(req);
+    return res.status(234).send("MERN");
 });
 
-//Route for save a new book
-app.post("/books", async (req, res) => {
-    try {
-        if (!req.body.title || !req.body.author || !req.body.publishYear) {
-            return res.status(400).send({message: "Send all required fields: title, author, publishYear",});
-        }
-        const newBook = {
-            title: req.body.title, 
-            author: req.body.author,
-            publishYear: req.body.publishYear,
-        };
-        const book = await Book.create(newBook);
-
-        return res.status(201).send(book);
-    } catch (error) {
-        console.log(error.message);
-        res.status(500).send({message: error.message});
-    }
-});
-// Get All Books 
-app.get("/books", async (req, res) => {
-    try {
-        const books = await Book.find({});
-
-        return res.status(200).json({
-            count: books.length,
-            data: books
-        });
-    } catch (error) {
-        console.log(error.message);
-        res.status(500).send({message: error.message});
-    }
-});
-
+app.use('/books', routesBooks);
 
 mongoose
     .connect(MONGO_URL)
