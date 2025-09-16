59ef2c125a3fdc01be23dee1a8e0341f61c2db1a
Sara18Santos <sataide.santos@gmail.com>
2025-09-16 11:06:28 +0100

    Revert "Refactors Spinner import statements for consistency"

--- frontend/src/pages/ShowBook.jsx
+++ frontend/src/pages/ShowBook.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { useParams } from "react-router-dom";
 import BackButton from "../components/backButton";
-import Spinner from "../components/Spinner.jsx";
+import Spinner from "../components/spinner.jsx";
 
 const ShowBook = () => {
     const [book, setBook] = useState({});
